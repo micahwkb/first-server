3c326cbe0f193741e3239e6fdf8972a05c0c76b3
Micah Britton <micahwkb@gmail.com>
2016-11-08 11:55:49 -0500

    added requestHandler function for basic routing

--- app.js
+++ app.js
@@ -10,3 +10,14 @@ var server = http.createServer(requestHandler);
 server.listen(PORT, () => {
   console.log(`Server listening on: http://localhost:${PORT}`)
 });
+
+function requestHandler(request, response) {
+  if (request.url == "/") {
+    response.end("Welcome!");
+  } else if (request.url == "/urls") {
+    response.end("www.plentyofcats.ca\nwww.google.ca/#q=cats")
+  } else {
+    response.statusCode = 404;
+    response.end("Unknown Path");
+  }
+}
